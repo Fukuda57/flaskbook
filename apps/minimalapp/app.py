25a1a3b72cd31722c47d9f37e3cd19c99dece6b2
Fukuda57 <joichiro.57.f@icloud.com>
2022-02-14 14:00:56 +0900

    feat/template,Jinja2,url_for

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -8,6 +8,20 @@ def index():
     return "Hello, flask!"
 
 
-@app.route("/hello/<name>")
+@app.route("/hello/<name>", endpoint="hello-endpoint")
 def hello(name):
     return f"Hello,{name}!"
+
+
+@app.route("/name/<name>")
+def show_name(name):
+    return render_template("index.html", name=name)
+
+
+with app.test_request_context():
+    # /
+    print(url_for("index"))
+    # /hello/world
+    print(url_for("hello-endpoint", name="world"))
+    # /name/jou?page=1
+    print(url_for("show_name", name="jou", page="1"))
